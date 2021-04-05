d7a234b9012ec89500ef2675db48f3946eb58e67
Richard Crooks <email@rocrooks.co.uk>
2021-04-04 22:10:23 -0400

    Make functions to create form with text inputs

---
+++ functions/form-functions.php
@@ -0,0 +1,67 @@
+<?php
+//---FunctionBreak---
+/*Create a textbox for a form input
+
+$options is an array of options
+$options['Label'] is the label assigned to the input (required)
+$options['Name'] is the name of the input (required)
+$options['LabelSize'] is the size of the label (required)
+$options['BoxSize'] is the size of the box column (required)
+$options['Default']
+$options['Max']
+$options['Size']
+
+Returns the HTML for a textbox*/
+//---DocumentationBreak---
+function formtextbox($options)
+  {
+  $outputhtml = '<div style="display: table-row;">';
+  //Make label cell
+  $outputhtml = $outputhtml . '<div style="display: table-cell; width: ' . $options['LabelSize'] . ';">';
+  $outputhtml = $outputhtml . $options['Label'];
+  $outputhtml = $outputhtml . '</div>';
+  //Make text area cell
+  $outputhtml = $outputhtml . '<div style="display: table-cell; width: ' . $options['BoxSize'] . ';">';
+  $outputhtml = $outputhtml . '<input type="textbox" name="' . $options['Name'] . '"';
+  //Add the options to the textbox
+  if (isset($options['Default']) == true)
+    $outputhtml = $outputhtml . ' value="' . $options['Default'] . '"';
+  if (isset($options['Max']) == true)
+    $outputhtml = $outputhtml . ' value="' . $options['Default'] . '"';
+  if (isset($options['Length']) == true)
+    $outputhtml = $outputhtml . ' value="' . $options['Default'] . '"';
+  $outputhtml = $outputhtml . '>';
+  $outputhtml = $outputhtml . '</div>';
+  $outputhtml = $outputhtml . '</div>';
+
+  return $outputhtml;
+  }
+//---FunctionBreak---
+/*Create an HTML form
+
+$action is the action of the form
+$method is method is the method of the form
+$inputs is an array of the inputs for the form
+
+Returns the HTML for a textbox*/
+//---DocumentationBreak---
+function createform($action,$method,$inputs)
+  {
+  //Create form
+  $outputhtml = '<form action="' . $action . '" method="' . $method . '">';
+
+  $outputhtml = $outputhtml . '<div style="display: table;">';
+  //Make each input HTML
+  foreach($inputs as $input)
+    {
+    $inputhtml = formtextbox($input);
+    $outputhtml = $outputhtml . $inputhtml;
+    }
+  $outputhtml = $outputhtml . '</div>';
+
+  //Make submit and reset buttons, and close form
+  $outputhtml = $outputhtml . '<p><input type="submit" name="submit" value="Submit"> <input type="reset" name="reset" value="Reset"></p>'
+  $outputhtml = $outputhtml . '</form>';
+  }
+//---FunctionBreak---
+?>
